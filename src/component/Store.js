1dcfee16f2939fa1698996c306b07223bedc3b92
admin <admin@admins-MacBook-Pro.local>
2024-12-11 23:23:16 +0530

    Locate Places - Frontend - 3

--- src/component/Store.js
+++ src/component/Store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import rootReducer from './rootReducer';
-import { legacy_createStore } from '@reduxjs/toolkit/react';
+import { legacy_createStore } from '@reduxjs/toolkit';
 
 // Create the Redux store and apply thunk middleware
 const Store = legacy_createStore(
